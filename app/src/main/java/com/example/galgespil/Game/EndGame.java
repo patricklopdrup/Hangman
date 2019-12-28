c4ba3e8b0393d5ab68c1ce5e777769d336e0960e
plopdrup <s185092@student.dtu.dk>
2019-12-28 15:14:16 +0100

    filer i mapper

---
+++ app/src/main/java/com/example/galgespil/Game/EndGame.java
@@ -0,0 +1,46 @@
+package com.example.galgespil.Game;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.galgespil.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EndGame extends AppCompatActivity {
+    private boolean winner;
+    private long timePassed;
+    private int guesses = 0;
+
+    public EndGame() {}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_end_game);
+        Intent intent = getIntent();
+
+        //getting values from the Game class when the game ended
+        guesses = intent.getIntExtra("guesses", -1);
+        winner = intent.getBooleanExtra("winner", false);
+
+        if (winner) {
+            timePassed = intent.getLongExtra("time", -1L);
+        }
+
+        //putting data in the bundle
+        Bundle bundle = new Bundle();
+        bundle.putLong("time2", timePassed);
+        bundle.putInt("guesses", guesses);
+
+        //passing the data from the bundle to our fragment
+        Winner_loser winner_loser = new Winner_loser();
+        winner_loser.setArguments(bundle);
+        System.out.println("min bundle: "+bundle.toString());
+
+        //go to the fragment
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.endGameFramelayout, new Winner_loser(winner))
+                .commit();
+    }
+}
---
+++ app/src/main/java/com/example/galgespil/Highscore/Highscore.java
@@ -0,0 +1,102 @@
+package com.example.galgespil.Highscore;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.galgespil.R;
+
+import java.util.List;
+
+public class Highscore extends AppCompatActivity {
+    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    //for this to work the "template" made in "list_element.xml" has to be "android:layout_height = "wrap_content"
+    //"match_parent" does not work!
+
+    private HighscoreLogic highscoreLogic = new HighscoreLogic();
+    private List<HighscoreObject> highscoreSorted;
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_highscore);
+
+        //getting my sorted highscoreList
+        highscoreSorted = highscoreLogic.getSortedHighscoreList(highscoreLogic.getHighscoreKey(), this);
+
+        recyclerView = new RecyclerView(this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+        setContentView(recyclerView);
+    }
+
+    RecyclerView.Adapter adapter = new RecyclerView.Adapter() {
+        //this inflate the "template" for how one element in the list looks like
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View itemView = getLayoutInflater().inflate(R.layout.list_element, parent, false);
+            return new RecyclerView.ViewHolder(itemView) {};
+        }
+
+        //here we add data to the "template" for every element in our array. We use "int position" for this
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+            //getting the views
+            TextView timeUsed = holder.itemView.findViewById(R.id.time_used);
+            TextView word = holder.itemView.findViewById(R.id.the_word);
+            ImageView image = holder.itemView.findViewById(R.id.image);
+            TextView pos = holder.itemView.findViewById(R.id.placement);
+            TextView guesses = holder.itemView.findViewById(R.id.guesses);
+
+            //switch for number 1,2 and 3. They have there own medal image.
+            switch (position) {
+                case 0:
+                    image.setVisibility(View.VISIBLE);
+                    pos.setVisibility(View.GONE);
+                    image.setImageResource(R.drawable.gold);
+                    break;
+                case 1:
+                    image.setVisibility(View.VISIBLE);
+                    pos.setVisibility(View.GONE);
+                    image.setImageResource(R.drawable.silver);
+                    break;
+                case 2:
+                    image.setVisibility(View.VISIBLE);
+                    pos.setVisibility(View.GONE);
+                    image.setImageResource(R.drawable.bronze);
+                    break;
+                default:
+                    image.setVisibility(View.GONE);
+                    pos.setVisibility(View.VISIBLE);
+                    pos.setText(getString(R.string.show_pos, (position+1)));
+                    break;
+            }
+            long timeInMs = highscoreSorted.get(position).getTime();
+            float timeInSec = ((float)timeInMs / 1000);
+            //if time is more than 60 sec, we write fx "1 min 14 sek"
+            if(timeInSec >= 60.0) {
+                timeUsed.setText(getString(R.string.show_time_used_min, (int)(timeInSec)/60, (int)(timeInSec)%60));
+            } else {
+                timeUsed.setText(getString(R.string.show_time_used, timeInSec));
+            }
+            //setting the specific word and amount of guesses
+            word.setText(getString(R.string.show_word, highscoreSorted.get(position).getWord()));
+            guesses.setText(getString(R.string.show_guesses, highscoreSorted.get(position).getGuesses()));
+        }
+
+        @Override
+        public int getItemCount() {
+            return highscoreSorted.size();
+        }
+    };
+
+}
