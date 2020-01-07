7fe81c4a274bca32df7656567f3a36d78cf060c7
patrick <s185092@student.dtu.dk>
2020-01-07 12:33:10 +0100

    lavet så title kan scroll hvis den er for lang

--- app/src/main/java/com/example/galgespil/Statistic/StatAct.java
+++ app/src/main/java/com/example/galgespil/Statistic/StatAct.java
@@ -5,14 +5,17 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.galgespil.R;
 
 public class StatAct extends AppCompatActivity implements View.OnClickListener {
 
     private Button moreStats;
+    private TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,10 @@ public class StatAct extends AppCompatActivity implements View.OnClickListener {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.add(R.id.statistics_frame, fragment);
         fragmentTransaction.commit();
+
+        title = findViewById(R.id.statistics_title);
+        //used to getting the title to scroll by it self
+        title.setSelected(true);
 
         moreStats = findViewById(R.id.change_stats_btn);
         moreStats.setOnClickListener(this);
