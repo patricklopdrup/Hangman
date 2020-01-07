8ccf3e2930a3156d4dbdb295dcad8b468fb56013
plopdrup <s185092@student.dtu.dk>
2020-01-07 13:59:16 +0100

    fixed challenge, så tekst ikke går ind over knappen

--- app/build (1)/generated/source/buildConfig/androidTest/debug/com/example/galgespil/test/BuildConfig.java
+++ app/build (1)/generated/source/buildConfig/androidTest/debug/com/example/galgespil/test/BuildConfig.java
@@ -5,7 +5,7 @@ package com.example.galgespil.test;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.example.galgespil.test";
+  public static final String APPLICATION_ID = "com.example.galgespil.final_keyboard";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
